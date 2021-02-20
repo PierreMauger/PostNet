689fbe598e6f20880e012ffa7c761b6c43291655
adrien busnel <abusnel@epitech.eu>
2021-02-20 15:48:53 +0100

    added profile

--- my-app/src/component/profile/profile.tsx
+++ my-app/src/component/profile/profile.tsx
@@ -2,4 +2,37 @@ import { ReactComponent } from "*.svg";
 import React from "react";
 import './profile.css'
 
-export default {};
+type Profileprops = {
+    imgProfileURL: string
+    profileName: string
+    profileID: number
+    profileMessage: string
+    usrMood: string
+}
+
+function ShowProfile(props: Profileprops) {
+    return (
+        <div>
+            <div className="profile">
+                <div className="profileInfo">
+                    <img className="photoProfile"
+                        src={props.imgProfileURL}
+                        alt={props.profileName}
+                    />
+                    <div className="profileName">
+                        {props.profileName}
+                        {props.profileID}
+                    </div>
+                </div>
+                <div className="mood">
+                    {props.usrMood}
+                </div>
+                <div className="description">
+                    {props.profileMessage}
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default ShowProfile;
